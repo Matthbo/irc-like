5809bb7d3d5dfd65cbbe06c31fe96bb165a217b0
Matthijs Booman <matthijs.booman@hotmail.com>
2018-05-10 01:34:38 +0200

    changed to client class & better event handling

--- client.js
+++ client.js
@@ -3,8 +3,8 @@ const net = require('net'),
   util = require('util')
 
 /*Constances*/
-const HOST = '192.168.16.120',
-  PORT = 1337
+const HOST = '127.0.0.1',
+  PORT = 6667
 
 /*CommandLine handler*/
 function sendCLRes(sender, ...data){
@@ -15,18 +15,48 @@ function log(...data){
   sendCLRes('client', ...data)
 }
 
-process.stdin.addListener("data", function(d) {
-  cmd = d.toString().trim();
+/*Client*/
+class Client {
+  constructor(){
+    this.socket = new net.Socket().connect(PORT, HOST)
 
-  switch(cmd){
-    default:
-      log("Unkown command")
-  }      
-})
+    this.eventHandler()
 
-/*Socket*/
-const socket = new net.Socket().connect(PORT, HOST);
+    process.stdin.addListener("data", (d) => this.commandHandler(d))
+  }
 
-socket.on('connect', () => {
-  log('Connected to', socket.remoteAddress())
-})
+  commandHandler(data){
+    const cmd = data.toString().trim();
+
+    switch(cmd){
+      case 'exit':
+      case 'stop':
+        this.stop()
+        break
+      default:
+        log("Unkown command")
+    }
+  }
+
+  eventHandler(){
+    this.socket.on('connect', () => {
+      log('Connected to', this.socket.address().address)
+    })
+
+    this.socket.on('error', (err) => {
+      log('Connection error:', err.message)
+    })
+
+    this.socket.on('close', (had_error) => {
+      if(!had_error) log('Connection to server closed')
+      process.exit(0)
+    })
+  }
+
+  stop(){
+    this.socket.end()
+  }
+
+}
+
+new Client()
